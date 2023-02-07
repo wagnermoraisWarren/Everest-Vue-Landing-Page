d067c69e876f4f3ae46065e52c7590f131e764f4
wagnemoraisWarren <wagnermoraiscnt@gmail.com>
2023-02-07 00:51:32 -0300

    feat: novas dependencias e imagem

--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueMask from 'v-mask'
+import Toast from 'vue-toastification'
+import "vue-toastification/dist/index.css";
 // Vue.config.productionTip = false
 
 Vue.use(VueMask);
+Vue.use(Toast, {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 3,
+  newestOnTop: true,
+  filterBeforeCreate: (toast, toasts) => {
+    if (toasts.filter(
+      t => t.type === toast.type
+    ).length !== 0) {
+      // Returning false discards the toast
+      return false;
+    }
+    // You can modify the toast if you want
+    return toast;
+  }
+});
 
 new Vue({
   router,
